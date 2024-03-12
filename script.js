ed6ba8e52eb05e176908a40108108202259402cf
Erick559 <erickungu82@gmail.com>
2024-03-12 13:49:14 +0300

    feat: Access the faq content section for the clicked section.

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ const accordionItems = document.querySelectorAll('.section-header')
 accordionItems.forEach(accordionItem => {
   accordionItem.addEventListener('click',function(){
     this.classList.toggle('active')
+
+    const sectionContent = this.nextElementSibling;
+    console.log(sectionContent)
   })
 })
